29f129b1c720f944c4d407597ee49b9618fe63e4
Anthony Wilde <Anthonywilde117.com>
2021-03-11 12:02:18 +1100

    backups

---
+++ Console.js
@@ -0,0 +1,11 @@
+const { Console } = require('console');
+const fs = require('fs')
+
+const output = fs.createWriteStream('./stdout.log');
+const errorOutput = fs.createWriteStream('./stderr.log');
+// Custom simple logger
+const logger = new Console({ stdout: output, stderr: errorOutput });
+// use it like console
+const count = 5;
+logger.log('count: %d', count);
+// In stdout.log: count 5
