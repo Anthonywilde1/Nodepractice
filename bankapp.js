28c2724b85a933ba79b7e295161e6847c8f425cb
Anthony Wilde <Anthonywilde117.com>
2021-03-15 12:33:52 +1100

    fixed up the bank app

--- bankapp.js
+++ bankapp.js
@@ -21,6 +21,7 @@ let accounts = {
 }
 
 let currentuser = ''
+let otheruser = ''
 let passwordattempt = 0
 
 function menu() {
@@ -37,7 +38,8 @@ function menu() {
                     checkbalance();
                     break;
                 case "transfer":
-                              ;
+                    transfer();
+                    break;
                 case "quit":
                     quit();
                     break;
@@ -53,15 +55,15 @@ function withdraw() {
     rl.question("How much are you looking to withdraw today?\n", (answer) => {
         let integer = 0
         integer = parseFloat(answer)
-        let value = accounts.currentuser.balance - integer
+        let value = accounts[currentuser].balance - integer
         if (value < 0){
             console.log("Insufficient Funds")
-            console.log(`Your balance is: ${accounts.currentuser.balance}`)
+            console.log(`Your balance is: ${accounts[currentuser].balance}`)
             withdraw();
         } else if (value >= 0) {
             console.log(`Withdrawing: ${integer}`)
-            accounts.currentuser.balance -= integer
-            console.log(`Balance remaining: ${accounts.currentuser.balance}`)
+            accounts[currentuser].balance -= integer
+            console.log(`Balance remaining: ${accounts[currentuser].balance}`)
             anythingelse()
         } else {
             console.log("Error: Please try again")
@@ -76,8 +78,11 @@ function anythingelse() {
         answer = answer.toLowerCase().replace(/\s/g, '')
         if (answer === "yes") {
             menu()
-        } else {
+        } else if (answer === "no") {
             quit()
+        } else {
+            console.log("Error: Try again")
+            anythingelse();
         }
     })
 }
@@ -92,14 +97,14 @@ function deposit() {
     rl.question("How much are you looking to deposit?\n", (answer) => {
         let integer = 0
         integer = parseFloat(answer)
-        accounts.currentuser.balance += integer
-        console.log(`Your new balance is: ${accounts.currentuser.balance}`)
+        accounts[currentuser].balance += integer
+        console.log(`Your new balance is: ${accounts[currentuser].balance}`)
         anythingelse()
     })
 }
 
 function checkbalance() {
-    console.log(`Your current balance is: ${accounts.currentuser.balance}`)
+    console.log(`Your current balance is: ${accounts[currentuser].balance}`)
     anythingelse();
 }
 
@@ -109,7 +114,7 @@ function login() {
     rl.question("Please enter your Account Name to continue\n", (answer) => {
         answer = answer.toLowerCase().replace(/\s/g, '')
         for (const [key] of Object.entries(accounts)) {
-            if (key === answer) {
+            if (answer === key) {
                 currentuser = answer
                 passwordpart();
             }
@@ -139,7 +144,35 @@ function passwordpart() {
                     }
                 })
             }
-        
+
+function transfer() {
+    rl.question('Who are you wanting to transfer too?\n', (answer) => {
+        answer = answer.toLowerCase().replace(/\s/g, '')
+        otheruser = answer
+        if (accounts[otheruser]) {
+                rl.question(`How much do you wish to transfer to ${otheruser}?\n`, (answer2) => {
+                    let integer = 0
+                    integer = parseFloat(answer2)
+                    console.log(integer)
+                    console.log(accounts[currentuser].balance)
+                    let value = accounts[currentuser].balance - integer
+                    console.log(value)
+                    if (value > 0) {
+                        accounts[currentuser].balance -= integer
+                        accounts[otheruser].balance += integer
+                        console.log(`Transfered ${integer} to ${otheruser}`)
+                        anythingelse();
+                    } else if (value < 0){
+                        console.log(`Error: Not enough money for transfer. Please try again`)
+                        transfer()
+                    }
+                })
+            } else {
+                console.log(`${otheruser} does not exist. Please try again`)
+                transfer()
+            }
+        }
+)}
         
 
 login();
